396f74f87b56a01cc34c65216096c8551b3b8899
Manuel Sanchez del Rio <srio@esrf.eu>
2016-09-13 12:29:50 +0200

    Update test_import_all.py

--- test_import_all.py
+++ test_import_all.py
@@ -7,15 +7,15 @@ import numpy
 print("numpy version:", numpy.__version__)
 import scipy
 print("scipy version:", scipy.__version__)
-import bottlechest
-import chardet
-import nose
-import jinja2
-import sphinx
-import recommonmark
-import numpydoc
-from bs4 import BeautifulSoup
-import xlrd
+# import bottlechest
+# import chardet
+# import nose
+# import jinja2
+# import sphinx
+# import recommonmark
+# import numpydoc
+# from bs4 import BeautifulSoup
+# import xlrd
 
 import orangecanvas
 
