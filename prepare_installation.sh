cd0021f9ba8b8fdf9265042ac17df4459425b69e
lucarebuffi <luca.rebuffi@elettra.eu>
2016-09-20 17:50:00 +0200

    checked linux installation with different versions

---
+++ prepare_installation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get remove python3-matplotlib
+sudo apt-get remove python3-scipy
+sudo apt-get remove python3-numpy
+
+sudo apt-get install python3-pip
+sudo pip3 install pip --upgrade
+
+if [[ `lsb_release -rs` == "14.04" ]]
+  sudo pip3 install virtualenv
+  sudo apt-get install python3.4-venv
+then
+  sudo apt-get install python3-virtualenv
+  sudo apt install virtualenv
+fi
+
+sudo apt-get install python3-pyqt4
+
+# SCIPY
+sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+
+# MATPLOTLIB 
+sudo apt-get install libfreetype6 libfreetype6-dev
+
+# XRAYLIB
+sudo apt-get install swig
+
+# PYMCA
+sudo apt-get install mesa-common-dev libgl1-mesa-dev freeglut3-dev
+
