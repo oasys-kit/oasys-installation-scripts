c788284d9db6c0f3ca11914bc5b7b3ed7ed6e66b
Manuel Sanchez del Rio <srio@esrf.eu>
2019-05-16 20:30:55 +0200

    updated to version 3.99

--- Developers/install_xraylib_from_github.sh
+++ Developers/install_xraylib_from_github.sh
@@ -16,12 +16,14 @@
 # clean old stuff
 
 #define python
-source oasys1env/bin/activate
-export PYTHON_SITE_PACKAGES=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+#source oasys1env/bin/activate
+export MYPYTHON=/Applications/Oasys1.1.app/Contents/MacOS/python
+export PYTHON_SITE_PACKAGES=`$MYPYTHON -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 
 echo "Cleaning old installation files..."
 rm -rf xraylib
-rm $PYTHON_SITE_PACKAGES/xray*.py
+rm $PYTHON_SITE_PACKAGES/xraylib*
+rm $PYTHON_SITE_PACKAGES/_xraylib*
 
 
 # xraylib
@@ -29,15 +31,20 @@ echo "Installing Oasys dependency xraylib"
 # curl -O http://lvserver.ugent.be/xraylib/xraylib-3.1.0.tar.gz
 # tar xvfz xraylib-3.1.0.tar.gz
 # cd xraylib-3.1.0
-git clone https://github.com/tschoonj/xraylib
+#git clone https://github.com/tschoonj/xraylib
+curl -O http://lvserver.ugent.be/xraylib/unstable/xraylib-3.99.0.tar.gz
+tar xvfz xraylib-3.99.0.tar.gz
+mv xraylib-3.99.0 xraylib
 cd xraylib
-autoreconf -i 
+#autoreconf -i 
+mkdir build
+cd build
 
-./configure --enable-python --enable-python-integration PYTHON=`which python` --disable-libxrl
+../configure --enable-python --enable-python-integration PYTHON=$MYPYTHON --disable-libxrl
 make
 cp python/xraylib.py $PYTHON_SITE_PACKAGES 
 cp python/.libs/*.so  $PYTHON_SITE_PACKAGES
-cp src/.libs/*.dylib  # for mac
-cp src/.libs/*.so  # for linux
-cd ..
+#cp src/.libs/*.dylib  # for mac
+#cp src/.libs/*.so  # for linux
+cd ../..
 echo "All done for xraylib. "
