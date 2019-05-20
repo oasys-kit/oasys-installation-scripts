bef4ca7b58835b7698c19ddf6bfb3799afc47b7d
Juan Reyes <juan.reyes-herrera@esrf.fr>
2019-05-20 15:04:12 +0200

    updated

--- Release_1.1/Miniconda/install_oasys1_MC.sh
+++ Release_1.1/Miniconda/install_oasys1_MC.sh
@@ -2,22 +2,22 @@
 
 #===============================================================================
 #
-# script to install OASYS in a LINUX Miniconda3 environment (sudo is not required)
+# script to install OASYS in a LINUX Miniconda3 environment 
+# (sudo is not required)
 #
-# Before starting make sure that your system has installed: 
-#      - python3
-#      - qt and pyqt
-#   If any of these items is not present in your system, contact the
-#   system administrator and ask for their installation.     
 #
-# To be sure, run the script "prepare_installation.sh".
+# Before starting, run the script "prepare_installation_MC.sh".
 #
-# Then place this script in a suitable directory (e.g., $HOME/OASYS_VE) and run
-# this script automatically (it may take a very long time) or manually by
-# copy/paste line by line, if you want to detect possible errors). 
+# Then run this script automatically (it may take a very long time) or 
+# manually by copy/paste line by line, if you want to detect possible errors. 
+#
+
+# Note that the script changes directory to the main installation directory. 
 #
 # Then start oasys by:
-#   ~/miniconda3/bin/python3 -m oasys.canvas -l4 --force-discovery
+#   python -m oasys.canvas -l4 --force-discovery
+# or
+#   ./start_oasys.sh
 #
 #===============================================================================
 #
@@ -30,15 +30,34 @@
 # proxy 
 #export all_proxy=http://proxy.esrf.fr:3128/
 
+cd ../..
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+
+# Note that by default miniconda3 directory is installed in the home directory ~/miniconda3
+# If wanted, you can define another working directory. 
+./Miniconda3-latest-Linux-x86_64.sh
+
+# copy start_oasys_MC.sh to .. and customize them if necessary
+
+cp  Release_1.1/Miniconda/start_oasys.sh .
+
+source ./define_environment.sh
+
 # xraylib
 echo "Installing Oasys dependency xraylib"
-$HOME/miniconda3/bin/conda install -c conda-forge xraylib=3.3.0
+conda install -c conda-forge xraylib=3.3.0
+
+# Qt specific version 5.11.3
+
+pip install pyqt5==5.11.3
 
 #
 # step 4 install oasys
 #
 echo "Installing Oasys..."
 
-$HOME/miniconda3/bin/python3 -m pip install oasys1
+pip install oasys1
 
-echo "All done. You can start Oasys using ../../start_oasys_MC.sh"
+echo "All done. You can start Oasys using start_oasys.sh and install add-ons from the Options in the menu"  
