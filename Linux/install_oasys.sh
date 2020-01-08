db819bba239f89e114e2ed76e894ac2b4e31a726
lucarebuffi <lrebuffi@anl.gov>
2020-01-08 14:20:38 -0600

    New Installation Scripts

--- Linux/install_oasys.sh
+++ Linux/install_oasys.sh
@@ -2,26 +2,30 @@
 
 type git >/dev/null 2>&1 || ./check_git.sh
 
-read -p "Delete or Rename previous Miniconda3 installation? ([D]/r)" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Rr]$ ]]
-then
-    mv $HOME/miniconda3 $HOME/miniconda3_$(date +'%d-%m-%Y_%H.%M.%S')
-else
-    rm -fR $HOME/miniconda3
-fi
+minicondadir=$HOME/miniconda3
 
-wget https://repo.continuum.io/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh
+[ -d "${minicondadir}" ] &&  {
+    read -p "Delete or Rename previous Miniconda3 installation? ([D]/r)" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Rr]$ ]]
+    then
+        mv $minicondadir $HOME/miniconda3_$(date +'%d-%m-%Y_%H.%M.%S')
+    else
+        rm -fR $minicondadir
+    fi
+}
+
+wget -O https://repo.continuum.io/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh
 chmod +x Miniconda3-4.7.12.1-Linux-x86_64.sh
 ./Miniconda3-4.7.12.1-Linux-x86_64.sh
 
-cd $HOME/miniconda3/bin
+cd $minicondadir/bin
 ./python -m pip install pip --upgrade
 ./conda install -c conda-forge xraylib=3.3.0
 ./python -m pip install oasys1
 cd -
 
-$HOME/miniconda3/bin/python -m oasys.canvas
+./start_oasys.sh
 
 read -p "Create Desktop Application (requires sudo grants)? ([Y]/n)" -n 1 -r
 echo    # (optional) move to a new line
