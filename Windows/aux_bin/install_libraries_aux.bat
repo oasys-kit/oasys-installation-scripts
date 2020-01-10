55e0168909972e232d6803f349859add4ae7823f
lucarebuffi <lrebuffi@anl.gov>
2020-01-10 15:20:57 -0600

    New Installation Scripts

--- Windows/aux_bin/install_libraries_aux.bat
+++ Windows/aux_bin/install_libraries_aux.bat
@@ -1,8 +1,8 @@
 @echo off
 
 pip install pip --upgrade
-cmd /c conda install -c conda-forge xraylib=3.3.0
 cmd /c conda install git
+cmd /c conda install -c conda-forge xraylib=3.3.0
 pip install oasys1 --upgrade
 python -m oasys.canvas -l4
 
