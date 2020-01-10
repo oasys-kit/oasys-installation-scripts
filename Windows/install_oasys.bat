4605e61fe048b947c128057a6d43cd80b7606ffa
lucarebuffi <lrebuffi@anl.gov>
2020-01-10 10:51:03 -0600

    New Installation Scripts

--- Windows/install_oasys.bat
+++ Windows/install_oasys.bat
@@ -9,18 +9,18 @@ if exist "C:%HOMEPATH%\Miniconda3" (
     set /p "user_decision=[r]ename, [d]elete or [l]eave it?>"
     
     if /I "!user_decision!"=="d" (
-        echo Deleting existing Miniconda
+        echo Deleting existing Miniconda ...
         rmdir /S /Q "C:%HOMEPATH%\Miniconda3"
     ) else (
 	if /I "!user_decision!"=="r" (
             for /f "tokens=*" %%a in ('powershell get-date -format "{yyyy-MM-dd_HH.mm.ss}"') do set td=%%a
-            echo Renaming existing Miniconda to Miniconda3_!td!
             rename "C:%HOMEPATH%\Miniconda3" Miniconda3_!td!
+            echo Existing Miniconda renamed to Miniconda3_!td!
         ) else (
 	    if /I "!user_decision!"=="l" (
                 goto install_oasys
             ) else (
-		echo Deleting existing Miniconda
+		echo Deleting existing Miniconda ...
         	rmdir /S /Q "C:%HOMEPATH%\Miniconda3" 
             )
         )
@@ -28,15 +28,15 @@ if exist "C:%HOMEPATH%\Miniconda3" (
 	
     if %ERRORLEVEL% GEQ 1 goto batch_exit
 ) else (
-    echo Miniconda distribution not found
+    echo Miniconda not found
 )
 
 :install_miniconda
 
 if exist "%cd%\Miniconda3-4.7.12.1-Windows-x86_64.exe" (
-    echo Miniconda Installer found
+    echo Miniconda Installer already downloaded
 ) else (
-    echo Downloading Miniconda Installer
+    echo Downloading Miniconda Installer ...
     powershell -Command Invoke-WebRequest https://repo.continuum.io/miniconda/Miniconda3-4.7.12.1-Windows-x86_64.exe -O Miniconda3-4.7.12.1-Windows-x86_64.exe
 )
 
