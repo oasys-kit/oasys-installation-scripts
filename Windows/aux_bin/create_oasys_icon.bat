582e5883916d3150c58b1955741bf979e72165e1
Luca Rebuffi <lrebuffi@anl.gov>
2021-02-09 13:01:35 -0600

    Update create_oasys_icon.bat

--- Windows/aux_bin/create_oasys_icon.bat
+++ Windows/aux_bin/create_oasys_icon.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%cd%\aux_bin\create_link.bat -linkfile "C:%HOMEPATH%\Desktop\Oasys 1.2.lnk" -target "C:%HOMEPATH%\Miniconda3\Scripts\activate.bat" -linkarguments "& python -m oasys.canvas" -iconlocation "%cd%\aux_bin\oasys.ico"
+%cd%\aux_bin\create_link.bat -linkfile "C:%HOMEPATH%\Desktop\Oasys 1.2.lnk" -target "C:%HOMEPATH%\Miniconda3\Scripts\activate.bat" -linkarguments "& python -m oasys.canvas --force-discovery" -iconlocation "%cd%\aux_bin\oasys.ico"
 
 
 
