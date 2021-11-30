7aceda066db652c0e1acff3596c063b0b8ba1b3b
lucarebuffi <lrebuffi@anl.gov>
2021-11-29 21:15:26 -0600

    bug fixing

--- Windows/aux_bin/install_libraries_aux1.2.bat
+++ Windows/aux_bin/install_libraries_aux1.2.bat
@@ -3,6 +3,8 @@
 pip install pip --upgrade
 cmd /c conda install git
 cmd /c conda install -c conda-forge xraylib=3.3.0
+pip install oasys-srwpy --upgrade
+pip install PyQtWebEngine==5.15.2 --upgrade
 pip install oasys1 --upgrade
 python -m oasys.canvas -l4
 
