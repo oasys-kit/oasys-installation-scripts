c1d2ebd9a4ec7363031b5f54f1347c11a9027390
lucarebuffi <lrebuffi@anl.gov>
2022-04-20 18:50:31 -0500

    bug fixing

--- Windows/aux_bin/create_oasys_icon1.2.bat
+++ Windows/aux_bin/create_oasys_icon1.2.bat
@@ -1,9 +1,19 @@
 @echo off
 
+set filelink = "C:%HOMEPATH%\Desktop\Oasys 1.2.lnk"
+
+if not exist "C:%HOMEPATH%\Desktop" (
+	echo Desktop folder is missing, looking for Desktop folder in Onedrive.
+	if exist "%OneDrive%\Desktop" (
+		set filelink="%OneDrive%\Desktop\Oasys 1.2.lnk"
+		)
+	)
+echo The link to be created is: %filelink%
+
 set curdir=%~s0
 set curdir=%curdir:~0,-12%
 
-%curdir%\create_link.bat -linkfile "C:%HOMEPATH%\Desktop\Oasys 1.2.lnk" -target "C:%HOMEPATH%\Miniconda3\Scripts\activate.bat" -linkarguments "& python -m oasys.canvas" -iconlocation "%curdir%\oasys.ico"
+%curdir%\create_link.bat -linkfile %filelink% -target "C:%HOMEPATH%\Miniconda3\Scripts\activate.bat" -linkarguments "& python -m oasys.canvas" -iconlocation "%curdir%\oasys.ico"
 
 
 
