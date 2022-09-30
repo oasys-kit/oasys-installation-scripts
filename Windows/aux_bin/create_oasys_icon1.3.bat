7bd73e4dd64fd12b5e061b74d0f73e92bf1cdc19
lucarebuffi <lrebuffi@anl.gov>
2022-09-30 12:00:37 -0500

    bug fixing

--- Windows/aux_bin/create_oasys_icon1.3.bat
+++ Windows/aux_bin/create_oasys_icon1.3.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%cd%\aux_bin\create_link.bat -linkfile "C:%HOMEPATH%\Desktop\Oasys 1.3.lnk" -target "%user_dir%\Miniconda3\Scripts\activate.bat" -linkarguments "& python -m oasys.canvas" -iconlocation "%cd%\aux_bin\oasys.ico"
+%cd%\aux_bin\create_link.bat -linkfile "%user_dir%\Desktop\Oasys 1.3.lnk" -target "%user_dir%\Miniconda3\Scripts\activate.bat" -linkarguments "& python -m oasys.canvas" -iconlocation "%cd%\aux_bin\oasys.ico"
 
 
 
