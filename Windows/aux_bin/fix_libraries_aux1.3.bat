500fb09969334269569c55336b7f759b1fbb7ae3
lrebuffi <lrebuffi@anl.gov>
2023-05-26 15:14:34 -0500

    bug fixing

--- Windows/aux_bin/fix_libraries_aux1.3.bat
+++ Windows/aux_bin/fix_libraries_aux1.3.bat
@@ -8,7 +8,8 @@ if %fix%==0 (
     pip uninstall -y oasys-srwpy
     pip install oasys-srwpy --upgrade
 ) else (
-    echo SRW not installed, yet
+    pip install oasys-srwpy --upgrade
+    REM echo SRW not installed, yet
 )
 
 python -c "import sys, pkgutil; print(0 if pkgutil.find_loader('PyQt5.QtWebEngineWidgets') else 1)" > fix.txt
@@ -19,8 +20,22 @@ if %fix%==0 (
     pip uninstall -y PyQtWebEngine
     pip install PyQtWebEngine==5.14.0
 ) else (
-    echo PyQtWebEngine not installed, yet
+    pip install PyQtWebEngine==5.14.0
+    REM echo PyQtWebEngine not installed, yet
 )
 
+
+REM powershell -Command Invoke-WebRequest https://anl.box.com/s/58enqcetmzta2w4lkxnjus71wpfz8e30 -O OASYS1-ShadowOui-Advanced-Tools-1.0.114.tar.gz
+
+REM python -c "import sys, pkgutil; print(0 if pkgutil.find_loader('orangecontrib.shadow_advanced_tools') else 1)" > fix.txt
+REM set /p fix=<fix.txt
+REM del fix.txt
+
+REM if %fix%==0 (
+REM     pip uninstall -y OASYS1-Shadow-Advanced-Tools
+REM     pip install OASYS1-ShadowOui-Advanced-Tools-1.0.114.tar.gz
+REM ) else (
+REM     pip install OASYS1-ShadowOui-Advanced-Tools-1.0.114.tar.gz
+REM )
 
 
