842df7fc7cbc30e6a94822b43aa293db8036dd2f
lucarebuffi <lrebuffi@anl.gov>
2023-05-28 16:37:51 -0500

    bug fixing

--- Windows/aux_bin/fix_BLOMS23_aux.bat
+++ Windows/aux_bin/fix_BLOMS23_aux.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell -Command Invoke-WebRequest https://anl.box.com/shared/static/58enqcetmzta2w4lkxnjus71wpfz8e30.gz -O OASYS1-ShadowOui-Advanced-Tools-1.0.114.tar.gz
+powershell -Command Invoke-WebRequest https://anl.box.com/shared/static/258velh41m5f29ppj49qsjl12oz715uy.gz -O OASYS1-ShadowOui-Advanced-Tools-1.0.115.tar.gz
 
 python -c "import sys, pkgutil; print(0 if pkgutil.find_loader('orangecontrib.shadow_advanced_tools') else 1)" > fix.txt
 set /p fix=<fix.txt
@@ -8,9 +8,9 @@ del fix.txt
 
 if %fix%==0 (
      pip uninstall -y OASYS1-Shadow-Advanced-Tools
-     pip install OASYS1-ShadowOui-Advanced-Tools-1.0.114.tar.gz
+     pip install OASYS1-ShadowOui-Advanced-Tools-1.0.115.tar.gz
 ) else (
-     pip install OASYS1-ShadowOui-Advanced-Tools-1.0.114.tar.gz
+     pip install OASYS1-ShadowOui-Advanced-Tools-1.0.115.tar.gz
 )
 
 
